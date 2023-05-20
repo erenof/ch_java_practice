17b340d67f19aeb2738eb03bd545f19d7f7fc774
Fernando Ereno <junglebz@gmail.com>
2023-05-19 21:12:02 -0300

    Subo Calse 9 - Workshop

---
+++ clase9.workshop/src/test/java/com/coderhouse/clase9/workshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.clase9.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
